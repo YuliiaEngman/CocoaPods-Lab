89691a49c5a55816f92c8af5120d2de901664723
Yuliia Engman <yuliia.engman@gmail.com>
2020-03-01 20:02:32 -0500

    added APIClient and UserModel

--- CocoaPods-Lab/APIClient.swift
+++ CocoaPods-Lab/APIClient.swift
@@ -8,3 +8,34 @@
 
 import Foundation
 import Alamofire
+
+struct APIClient {
+    
+    // Result<Cocktail, Error>
+    static func fetchUserIUnfo(completion: @escaping (AFResult<[User]>) -> ()) {
+        let endpointURLString = "https://randomuser.me/api/?results=500"
+        
+        // we need a URL
+        guard let url = URL(string: endpointURLString) else {
+            return
+        }
+        
+        AF.request(url).response { (response) in
+            //response.data
+            //response.error
+            //response.request
+            if let error = response.error {
+                completion(.failure(error))
+            } else if let data = response.data {
+                do {
+                    let results = try JSONDecoder().decode(ResultsWrapper.self, from: data)
+                    let users = results.results
+                    completion(.success(users))
+                } catch {
+                    //completion(.failure(error))
+                    print("\(error)")
+                }
+            }
+        }
+    }
+}
